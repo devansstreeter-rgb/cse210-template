012df5037b1503bc6e2175cc0b2e15da597544cb
devansstreeter-rgb <d.evans.streeter@gmail.com>
2026-06-29 22:50:23 -0700

    Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Diagnostics.Contracts;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade (percentage): ");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+        bool passGrade = true;
+        if (grade < 60)
+        {
+            letterGrade = "F";
+            passGrade = false;
+        } else
+        {
+            if (grade < 70)
+            {
+                letterGrade = "D";
+                passGrade = false;
+            }
+            else if (grade < 80)
+            {
+                letterGrade = "C";
+            }
+            else if (grade < 90)
+            {
+                letterGrade = "B";
+            }
+            else
+            {
+                letterGrade = "A";
+            }
+            int plusOrMinus = grade % 10;
+            if (grade < 95)
+            {
+                if (plusOrMinus >= 7)
+                {
+                    letterGrade += '+';
+                } 
+                else if (plusOrMinus < 3)
+                {
+                    letterGrade += '-';
+                }
+            }
+        }
+        Console.WriteLine($"Your grade is a/an {letterGrade}.");
+        if (passGrade)
+        {
+            Console.WriteLine("You pass the course!");
+        }
+        else
+        {
+            Console.WriteLine("Course Failed - but retakes are allowed!");
+        }
     }
 }
