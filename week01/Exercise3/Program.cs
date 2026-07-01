93c667a3e85c9aa641c8bae7e4862688ed3eef37
devansstreeter-rgb <d.evans.streeter@gmail.com>
2026-06-30 18:15:57 -0700

    Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 101);
+        int guessNum = -1;
+        int guessCount = 0;
+        while (guessNum != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guessNum = int.Parse(Console.ReadLine());
+            if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            guessCount++;
+        }
+        Console.WriteLine($"Guesses taken: {guessCount}");
     }
 }
