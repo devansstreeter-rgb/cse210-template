10efcad25cef869e547a0fc320524c08db053bfb
devansstreeter-rgb <d.evans.streeter@gmail.com>
2026-07-11 11:17:24 -0700

    Wk2 Learning Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "KenTaco Hut";
+        job1._jobTitle = "Rethermalizer Tester";
+        job1._startYear = 2001;
+        job1._endYear = 2002;
+
+        Job job2 = new Job();
+        job2._company = "Frozen Fury";
+        job2._jobTitle = "Soda Jerk";
+        job2._startYear = 1999;
+        job2._endYear = 2000;
+
+        Resume myResume = new Resume();
+        myResume._name = "Daniel Streeter";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,17 @@
+public class Resume {
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public Resume()
+    {     
+    }
+
+     public void Display()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        foreach (Job x in _jobs)
+        {
+            x.Display();
+        }
+    }
+}
