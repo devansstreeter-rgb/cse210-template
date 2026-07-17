c87d8ded916c55bf590696312a7ae7e217d96f30
devansstreeter-rgb <d.evans.streeter@gmail.com>
2026-07-16 21:45:13 -0700

    Week 3 Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction one = new Fraction();
+        Fraction five = new Fraction(5);
+        Fraction threeFourths = new Fraction(3,4);
+        Fraction oneThird = new Fraction(1,3);
+
+        Console.WriteLine(one.GetDecimalValue());
+        Console.WriteLine(five.GetDecimalValue());
+        Console.WriteLine(threeFourths.GetDecimalValue());
+        Console.WriteLine(oneThird.GetDecimalValue());
     }
 }
